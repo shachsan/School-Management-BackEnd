ae3da70cd8826c45adedcc5461d0e9f8763029e2
Sanjay Sai <shachsan@hotmail.com>
2019-01-20 21:27:09 -0500

    create destroy method implement

--- app/controllers/api/v1/lecture_schedules_controller.rb
+++ app/controllers/api/v1/lecture_schedules_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::LectureSchedulesController < ApplicationController
-
+    before_action :find_schedule, only: :destroy
     def index
         @schedules=LectureSchedule.all 
         render json: @schedules
@@ -10,9 +10,18 @@ class Api::V1::LectureSchedulesController < ApplicationController
         render json: @schedule
     end 
 
+    def destroy
+        @schedule.destroy
+        render json: @schedule
+    end 
+
 
     private
     def schedule_params
         params.permit(:event, :date, :day, :start_time, :end_time, :mod_id, :lecture_room_id)
     end 
+
+    def find_schedule
+        @schedule=LectureSchedule.find(params[:id])
+    end
 end
