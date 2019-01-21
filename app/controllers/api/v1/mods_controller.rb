a970c52467640d921dde27bcd5628d202c64d9c7
Sanjay Sai <shachsan@hotmail.com>
2019-01-21 00:32:18 -0500

    create serializer for mod

--- app/controllers/api/v1/mods_controller.rb
+++ app/controllers/api/v1/mods_controller.rb
@@ -1,2 +1,6 @@
 class Api::V1::ModsController < ApplicationController
+    def index
+        @mods=Mod.all 
+        render json: @mods
+    end
 end
--- app/serializers/mod_serializer.rb
+++ app/serializers/mod_serializer.rb
@@ -1,3 +1,3 @@
 class ModSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :start_date
 end
